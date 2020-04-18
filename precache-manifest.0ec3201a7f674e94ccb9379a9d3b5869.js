f5a8c1423dca6c882eb1c25bd86dc10f82b3d2c4
Russel Rodrigues <russel.rodrigues@sts.in>
2020-04-18 21:01:19 +0530

    Updates

---
+++ precache-manifest.0ec3201a7f674e94ccb9379a9d3b5869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "383e8d154731b3423843",
+    "url": "/reactjs-covid19/static/css/main.603ffea9.chunk.css"
+  },
+  {
+    "revision": "383e8d154731b3423843",
+    "url": "/reactjs-covid19/static/js/main.383e8d15.chunk.js"
+  },
+  {
+    "revision": "e00b98b046ee44305aa9",
+    "url": "/reactjs-covid19/static/js/1.e00b98b0.chunk.js"
+  },
+  {
+    "revision": "7169c97c9cce2ec41fe3",
+    "url": "/reactjs-covid19/static/js/runtime~main.7169c97c.js"
+  },
+  {
+    "revision": "5a88540a516f619bd4486b227ae73b8e",
+    "url": "/reactjs-covid19/index.html"
+  }
+];
